296f0df814c9a645faf40cff87e4cfe5e6250350
Jacobus Burger <therealjacoburger@gmail.com>
2020-03-01 14:38:31 -0800

    added graphics.c

---
+++ src/graphics.c
@@ -0,0 +1,28 @@
+// Main file for ncurses display management (and also input for the moment)
+#include <ncurses.h>
+#include "maps.c"
+#include "elog.c"
+
+// render the start menu
+void
+render_menu(void);
+
+// render the game screen
+void
+render_game(void);
+
+// print board info (this player's board info)
+void
+draw_board(board B);
+
+// print radar info (known enemy board info)
+void
+draw_radar(radar R);
+
+// print elog info to ncurses elog
+void
+draw_elog(elog E);
+
+// echo user input string or err/response string to input bar
+void
+echo_input(char str[static ENTRY_LENGTH]);

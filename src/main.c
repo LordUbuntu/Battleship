fa52d2e8577d0cfdbf9a9615b89809d6e1287742
Jacobus Burger <therealjacoburger@gmail.com>
2023-02-11 18:37:31 -0800

    added plans for design

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include "network.c"
+
+
+int main(void) {
+        // init curses graphics
+        // render main screen
+        //   if user selects single player
+        //     render game screen
+        //     start bot
+        //     return to main screen when game is done
+        //   if player selects network player
+        //     render connection screen
+        //     if player successfully connects to another player
+        //       render game screen
+        //       return to main screen when game is done
+        //   if player selects help, render help screen
+        //   if player selects quit, end program
+}

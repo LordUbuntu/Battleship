4c04a5512ddd03ee094b908990b24cc9771766e5
Jacobus Burger <therealjacoburger@gmail.com>
2023-10-02 14:50:18 -0700

    feature: render ships

--- src/graphics.c
+++ src/graphics.c
@@ -1,4 +1,8 @@
 #include <ncurses.h>
+#include "data.h"
+
+
+#define SHIP_CHAR '#'
 
 
 int
@@ -16,8 +20,25 @@ init_ncurses(void)
 
 
 int
+render_ships(ship *ships) {
+        // iterate over each ship, drawing its position relative to (0,0)
+        for (int i = 0; i < NUM_SHIPS; i++) {
+                ship s = ships[i];
+                int x, y = s.front.x, s.front.y;
+                for(int x, y = s.front.x, s.front.y
+                   ;x < s.back.x, y < s.back.y
+                   ;x++, y++) {
+                        mvaddch(y, x, SHIP_CHAR);
+                        getch();  // for debugging
+                }
+        }
+}
+
+
+int
 stop_ncurses(void)
 {
+        clear();
         curs_set(TRUE);
         echo();
         nocbreak();

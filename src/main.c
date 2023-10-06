a2d937cee20f0159430dadd5e3c225db91b1279f
Jacobus Burger <therealjacoburger@gmail.com>
2023-10-05 21:18:06 -0700

    refactor: added a way to convert map coordinate to cartesian

--- src/main.c
+++ src/main.c
@@ -20,6 +20,21 @@ void init_ships(ship *ships_arr) {
 }
 
 
+int char_to_pos(char msg[static 3], pos *p) {
+        int y = msg[0] - 0x41;  // letter part A-J
+        int x = msg[1] - 0x30;      // number part 0-9
+        // return error if conversion data invalid
+        if (y < 0 || y > 9)
+                return 1;
+        if (x < 0 || x > 9)
+                return 1;
+        // set coordinates
+        p->x = x;
+        p->y = y;
+        return 0;
+}
+
+
 int main(void) {
         // init curses graphics
         init_ncurses();
@@ -34,21 +49,24 @@ int main(void) {
         // render empty input box
         WINDOW *text_box = newwin(3, COLS, LINES - 3, 0);
         wborder(text_box, '|', '|', '-', '-', '+', '+', '+', '+');
-        wmove(text_box, 1, 1);
-        wprintw(text_box, "Coordinate...");
         wrefresh(text_box);
 
         // get input box input
         char msg[3];
         wgetstr(text_box, msg);
-        msg[2] = '\n';
         wmove(text_box, 1, 1);
         wprintw(text_box, "Strike: %.2s", msg);
         wrefresh(text_box);
 
         // show actual coordinates
         wmove(text_box, 1, 1);
-        wprintw(text_box, "Coordinate is: %iy %ix", (msg[0] - 0x41 + 1), (msg[1] - 0x30));
+        pos p = { 1, 1 };
+        int status = char_to_pos(msg, &p);
+        if (status) {
+                wprintw(text_box, "conversion failed! %i", status);
+        } else {
+                wprintw(text_box, "Coordinate is: %iy %ix", p.y, p.x);
+        }
         wrefresh(text_box);
 
         // initialize and render ships

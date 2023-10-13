4c81d3975b7239528487aaacbf7c9fa36c35162c
Jacobus Burger <therealjacoburger@gmail.com>
2023-10-12 20:45:32 -0700

    refactor: moved routines around with prototypes to improve readability

--- src/main.c
+++ src/main.c
@@ -5,88 +5,8 @@
 #include "data.h"
 
 
-int menu(void) {
-        clear();
-        refresh();
-        int input = 0;
-        WINDOW *menu = newwin(LINES, COLS, 0, 0);
-        keypad(menu, true);
-        char option[4][16] = {
-                "Single Player",
-                "Multi Player",
-                "Help",
-                "Quit",
-        };
-        int highlight = 0;  // highlighted menu item
-        // menu loop
-        bool loop = true;
-        while (loop) {
-                mvwprintw(menu, 3, 5, "MENU");
-                for (int i = 0; i < 4; i++) {
-                        if (i == highlight)
-                                wattron(menu, A_REVERSE);
-                        mvwprintw(menu, 3+(2*(i+1)), 3, "%s", option[i]);
-                        wattroff(menu, A_REVERSE);
-                }
-                mvwprintw(menu, 10, 10, "%i", input);
-                wrefresh(menu);
-                input = wgetch(menu);
-                switch (input) {
-                        case KEY_DOWN:
-                                highlight = (highlight + 1) % 4;
-                                break;
-                        case KEY_UP:
-                                highlight = (highlight - 1) % 4;
-                                highlight = highlight < 0 ? 3 : highlight;
-                                break;
-                        case '\n':
-                                loop = false;
-                                break;
-                        default:
-                                break;
-                }
-        }
-        delwin(menu);
-        erase();
-        refresh();
-        return highlight;
-}
-
-
-// BUG: why is blank on first vision?
-void help(void) {
-        // this is fine because the help page data is static
-        clear();
-        refresh();
-        WINDOW *help = newwin(LINES, COLS, 0, 0);
-        int input = 0;
-        while (true) {
-                if (input == 'q')
-                        break;
-                mvwprintw(help, 1, 1, "The goal of battleship is to sink all");
-                mvwprintw(help, 2, 1, "of your opponent's pieces before they");
-                mvwprintw(help, 3, 1, "sink yours.");
-
-                mvwprintw(help, 5, 1, "First, place your pieces on your board");
-                mvwprintw(help, 6, 1, "using the arrow keys, and ENTER to");
-                mvwprintw(help, 7, 1, "confirm your placement. Ensure that");
-                mvwprintw(help, 8, 1, "all pieces are not overlapping and");
-                mvwprintw(help, 9, 1, "are not out of bounds.");
-                mvwprintw(help, 10, 1, "Next, the game starts and you both");
-                mvwprintw(help, 11, 1, "take turns using the arrow keys to");
-                mvwprintw(help, 12, 1, "select an open spot on the opponent");
-                mvwprintw(help, 13, 1, "board, and firing a volley with ENTER.");
-                
-                mvwprintw(help, 15, 1, "To return to the menu, press 'q'");
-
-                wrefresh(help);
-
-                input = getch();
-        }
-        delwin(help);
-        erase();
-        refresh();
-}
+int menu(void);
+void help(void);
 
 
 int main(void) {
@@ -189,4 +109,88 @@ int main(void) {
         //   if player selects quit, end program
         // stop curses graphics
 */
+}
+
+
+int menu(void) {
+        clear();
+        refresh();
+        int input = 0;
+        WINDOW *menu = newwin(LINES, COLS, 0, 0);
+        keypad(menu, true);
+        char option[4][16] = {
+                "Single Player",
+                "Multi Player",
+                "Help",
+                "Quit",
+        };
+        int highlight = 0;  // highlighted menu item
+        // menu loop
+        bool loop = true;
+        while (loop) {
+                mvwprintw(menu, 3, 5, "MENU");
+                for (int i = 0; i < 4; i++) {
+                        if (i == highlight)
+                                wattron(menu, A_REVERSE);
+                        mvwprintw(menu, 3+(2*(i+1)), 3, "%s", option[i]);
+                        wattroff(menu, A_REVERSE);
+                }
+                mvwprintw(menu, 10, 10, "%i", input);
+                wrefresh(menu);
+                input = wgetch(menu);
+                switch (input) {
+                        case KEY_DOWN:
+                                highlight = (highlight + 1) % 4;
+                                break;
+                        case KEY_UP:
+                                highlight = (highlight - 1) % 4;
+                                highlight = highlight < 0 ? 3 : highlight;
+                                break;
+                        case '\n':
+                                loop = false;
+                                break;
+                        default:
+                                break;
+                }
+        }
+        delwin(menu);
+        erase();
+        refresh();
+        return highlight;
+}
+
+
+// BUG: why is blank on first vision?
+void help(void) {
+        // this is fine because the help page data is static
+        clear();
+        refresh();
+        WINDOW *help = newwin(LINES, COLS, 0, 0);
+        int input = 0;
+        while (true) {
+                if (input == 'q')
+                        break;
+                mvwprintw(help, 1, 1, "The goal of battleship is to sink all");
+                mvwprintw(help, 2, 1, "of your opponent's pieces before they");
+                mvwprintw(help, 3, 1, "sink yours.");
+
+                mvwprintw(help, 5, 1, "First, place your pieces on your board");
+                mvwprintw(help, 6, 1, "using the arrow keys, and ENTER to");
+                mvwprintw(help, 7, 1, "confirm your placement. Ensure that");
+                mvwprintw(help, 8, 1, "all pieces are not overlapping and");
+                mvwprintw(help, 9, 1, "are not out of bounds.");
+                mvwprintw(help, 10, 1, "Next, the game starts and you both");
+                mvwprintw(help, 11, 1, "take turns using the arrow keys to");
+                mvwprintw(help, 12, 1, "select an open spot on the opponent");
+                mvwprintw(help, 13, 1, "board, and firing a volley with ENTER.");
+                
+                mvwprintw(help, 15, 1, "To return to the menu, press 'q'");
+
+                wrefresh(help);
+
+                input = getch();
+        }
+        delwin(help);
+        erase();
+        refresh();
 }

7c9a46759adda72bad255663c814407043bd5a29
Jacobus Burger <therealjacoburger@gmail.com>
2023-10-13 16:57:34 -0700

    dev: commented on networking source file

---
+++ src/network.c
@@ -0,0 +1,8 @@
+/* Jacobus Burger (2023)
+ * This source file handles peer-to-peer networking and associated
+ * ADT's for both single/multi-player games of battleship. The
+ * former case uses localhost for networking, while the latter
+ * looks for available clients advertising themselves.
+ *
+ * This us currently a WIP
+ */

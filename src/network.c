3d50661a85b28212bc5ce6e638d7c44f6d4bd84b
Jacobus Burger <therealjacoburger@gmail.com>
2025-06-08 17:41:26 -0700

    coming back to attack the network implementation

--- src/network.c
+++ src/network.c
@@ -1,21 +1,7 @@
-/* Jacobus Burger (2023)
- * This source file handles peer-to-peer networking and associated
- * ADT's for both single/multi-player games of battleship. The
- * former case uses localhost for networking, while the latter
- * looks for available clients advertising themselves.
- *
- * This us currently a WIP
- *
- * Resources:
- * - Beej's Guide to Network Programming
- *   https://beej.us/guide/bgnet/html/split/index.html
- * - Socket Programming in C for Beginners
- *   https://www.youtube.com/watch?v=KEiur5aZnIM
- * References:
- * - Simple netcat in C
- *   https://github.com/guzlewski/netcat
- *
- * Design Plan:
- * The goal of the networking is a simple LAN network application.
- * Each move, A sends a packet of the move they made to B. That simple.
- */
+/*
+* Jacobus Burger (2025)
+* Coming back to this after so long. Will be quick to implement a simple
+*       message-passing networking feature over LAN and maybe also WAN
+*
+*
+*/

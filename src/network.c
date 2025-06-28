293455acde373a9e4d3a454a3ad5711167e93fa9
Jacobus Burger <therealjacoburger@gmail.com>
2025-06-27 23:41:25 -0700

    add resource

--- src/network.c
+++ src/network.c
@@ -2,6 +2,7 @@
 * Jacobus Burger (2025)
 * Coming back to this after so long. Will be quick to implement a simple
 *       message-passing networking feature over LAN and maybe also WAN
-*
-*
+* 
+* Helpful resources for anyone doing networking in C:
+* - https://beej.us/guide/bgnet/html/
 */

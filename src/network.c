b886ce1e059eebd1f6ed43920f08f4b5428a2335
Jacobus Burger <therealjacoburger@gmail.com>
2026-08-25 23:23:41 -0700

    getting back into the habit

--- src/network.c
+++ src/network.c
@@ -6,3 +6,24 @@
 * Helpful resources for anyone doing networking in C:
 * - https://beej.us/guide/bgnet/html/
 */
+// 2026-08-25, back again to start. This is a good project to get back into habit. Gonna implement LAN multiplayer.
+// thx to "Think and Learn" channel, actually useful socket programming tutorial
+// start this file just as standalone like a main.c file
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+
+int main(int argc, char *argv[])
+{
+        int sockfd, newsockfd, portno;
+        socklen_t clilen;
+        socklen_t srvlen;
+        char buf[1024]; // may be excessive but can shrink down later alongside encoding methods
+        // no 'return 0;' needed under normal program end conditions for C99+
+        printf("closed");
+}
